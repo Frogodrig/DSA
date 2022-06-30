f18be1c6030f0ba556093dc491094b0028cdc324
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2022-06-30 18:06:47 +0530

    understood aggressive cows

--- Binary-Search/aggressiveCows.cpp
+++ Binary-Search/aggressiveCows.cpp
@@ -38,11 +38,16 @@ int aggressiveCows(int arr[], int n, int k) {
     int ans = -1;
 
     while(start <= end) {
-        if(isPossible(arr, n, k, mid)) {
+
+        if(isPossible(arr, n, k, mid)) { //ex: 2 cows not fighting at dist. 4 will also not fight at dist. 5,6,7.....
+
             ans = mid;
-            start = mid + 1; //Because we aspire for the maximum distance
-        } else {
+            start = mid + 1; //Because we aspire for the maximum distance. Therefore, going to the right side.
+
+        } else { //ex: Not mandatory for 2 cows to not fight at dist. 4 and also not fight at dist. < 4
+
             end = mid - 1;
+
         }
 
         mid = start + (end-start)/2;
@@ -54,5 +59,7 @@ int aggressiveCows(int arr[], int n, int k) {
 
 int main(){
     
+    int a[] = {1,2,3,4,6};
+    cout<<aggressiveCows(a, 5, 2)<<endl;
     return 0;
 }
