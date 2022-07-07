b90b10208c1f6307eac4249700dcb75e5c316116
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2022-07-07 23:13:29 +0530

    Subsequences

---
+++ recursion/subsequences.cpp
@@ -0,0 +1,48 @@
+//powerset of "abc"
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+void solve(string str, string output, int index, vector<string> &ans) {
+
+    if(index >= str.length()) {
+
+        ans.push_back(output);
+        return;
+    }
+
+    //exclude
+    solve(str, output, index+1, ans);
+
+    //include
+    char ch = str[index];
+    output.push_back(ch);
+    solve(str, output, index+1, ans);
+}
+
+
+vector<string> subsequences(string str) {
+
+    vector<string> ans;
+    string output = "";
+    int index = 0;
+
+    solve(str, output, index, ans);
+    return ans;
+}
+
+
+int main() {
+    
+    string str = "abc";
+
+    vector<string> ans = subsequences(str);
+
+    for(int i=0;i<ans.size();i++) {
+        cout<<ans[i]<<" ";
+    }cout<<endl;
+
+    return 0;
+}
+
