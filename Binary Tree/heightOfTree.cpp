67ca23e8525993d7c6b3d07a387b1103c3661125
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2022-07-17 02:38:56 +0530

    Height of Tree

---
+++ Binary Tree/heightOfTree.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+class Node {
+
+    public: 
+        int data;
+        Node* left;
+        Node* right;
+
+   Node(int data) {
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+   }     
+
+};
+
+int height(Node* root) {
+    //base case
+    if(root == NULL) {
+        return 0;
+    }
+
+    int left = height(root -> left);
+    int right = height(root -> right);
+
+    int ans = max(left, right) + 1;
+    
+    return ans;
+}
