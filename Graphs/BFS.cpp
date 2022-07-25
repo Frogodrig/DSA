301c7ed43aa62ce55ab69122e936bf2f6f6ae759
Frogodrig <72153510+Frogodrig@users.noreply.github.com>
2022-07-26 01:20:10 +0530

    Breadth First Search

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<queue>
+#include<unordered_map>
+#include<list>
+using namespace std;
+
+void prepareAdjList(unordered_map<int, list<int>> &adjList, vector<pair<int, int>> &edges) {
+
+    for(int i=0; i<edges.size(); i++) {
+        int u = edges[i].first;
+        int v = edges[i].second;
+
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+}
+
+void bfs(unordered_map<int, list<int>> &adjList, unordered_map<int, bool> &visited, vector<int> &ans, int node) {
+
+    queue<int> q;
+    q.push(node);
+    visited[node] = true;
+
+    while(!q.empty()) {
+        int frontNode = q.front();
+        q.pop();
+
+        //store frontNode in answer
+        ans.push_back(frontNode);
+
+        //traverse all neighbours of frontNode
+        for(auto i: adjList[frontNode]) {
+            if(!visited[i]) {
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+
+vector<int> BFS(int vertex, vector<pair<int, int>> edges) {
+
+    unordered_map<int, list<int>> adjList;
+    vector<int> ans;
+    unordered_map<int, bool> visited;
+
+    prepareAdjList(adjList, edges);
+
+    //traverse all components: for disconnected graphs
+    for(int i=0; i<vertex; i++) {
+        if(!visited[i]) {
+            bfs(adjList, visited, ans, i);
+        }
+    }
+
+    return ans;
+}
+
